c8ccb5a7ad7108ac1ff19ad9c07a7827db7daf98
Md Khan <khan4019@gmail.com>
2014-02-09 19:25:25 -0600

    execute by onClick

--- calculator/src/com/thatjsdude/calculator/MainActivity.java
+++ calculator/src/com/thatjsdude/calculator/MainActivity.java
@@ -2,16 +2,21 @@ package com.thatjsdude.calculator;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.view.View.OnClickListener;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 	
 	EditText etOp1, etOp2;
 	TextView txtResult;
+	Button btnPlus, btnMinus, btnMulti, btnDivison;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,15 @@ public class MainActivity extends Activity {
 		etOp1 = (EditText)findViewById(R.id.etOperand1);
 		etOp2 = (EditText)findViewById(R.id.etOperand2);
 		txtResult = (TextView)findViewById(R.id.tvOutput);
+		btnPlus = (Button)findViewById(R.id.btnPlus);
+		btnMinus = (Button)findViewById(R.id.btnMinus);
+		btnMulti = (Button)findViewById(R.id.btnMulti);
+		btnDivison = (Button)findViewById(R.id.btnDivision);
+		
+		btnPlus.setOnClickListener(this);
+		btnMinus.setOnClickListener(this);
+		btnMulti.setOnClickListener(this);
+		btnDivison.setOnClickListener(this);
 	}
 
 	@Override
@@ -29,57 +43,39 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
-	public void add(View v) {
+	@Override
+	public void onClick(View v) {
+		
 		String op1Str = etOp1.getText().toString();
 		String op2Str = etOp2.getText().toString();
 		
 		if(!op1Str.equals("") && !op2Str.equals("")){
 			double operand1 = Double.parseDouble(op1Str);
 			double operand2 = Double.parseDouble(op2Str);		
-			double result = operand1 + operand2;		
+			double result = 0;			
+			switch(v.getId()){
+			
+			case R.id.btnPlus:
+				result = operand1 + operand2;
+				break;
+			case R.id.btnMinus:
+				result = operand1 - operand2;
+				break;
+			case R.id.btnMulti:
+				result = operand1 * operand2;
+				break;
+			case R.id.btnDivision:
+				result = operand1 / operand2;
+				break;
+			default:
+				break;
+			}
+			txtResult.setTextColor(Color.BLACK);
 			txtResult.setText("Result = "+result);
 		}
 		else{
+			txtResult.setTextColor(Color.RED);
 			txtResult.setText("Please insert both the operands");
-		}
-		
-				
-	}
-	
-	public void subtract(View v){
-		String op1Str = etOp1.getText().toString();
-		String op2Str = etOp2.getText().toString();
-		
-		if(!op1Str.equals("") && !op2Str.equals("")){
-			double operand1 = Double.parseDouble(op1Str);
-			double operand2 = Double.parseDouble(op2Str);		
-			double result = operand1 - operand2;		
-			txtResult.setText("Result = "+result);
-		}
-		else{
-			txtResult.setText("Please insert both the operands");
-		}
-	}
-	public void multi(View v){
-		String op1Str = etOp1.getText().toString();
-		String op2Str = etOp2.getText().toString();
-		
-		double operand1 = Double.parseDouble(op1Str);
-		double operand2 = Double.parseDouble(op2Str);
-		
-		double result = operand1 * operand2;
-		
-		txtResult.setText("Result = "+result);
-	}
-	public void divison(View v){
-		String op1Str = etOp1.getText().toString();
-		String op2Str = etOp2.getText().toString();
-		
-		double operand1 = Double.parseDouble(op1Str);
-		double operand2 = Double.parseDouble(op2Str);
-		
-		double result = operand1 / operand2;
-		
-		txtResult.setText("Result = "+result);
+		}		
 	}
 }
